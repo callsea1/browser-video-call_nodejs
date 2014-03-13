11eda5a9f45abca0d8f20d6d49cdc8859f34b450
Mattox Beckman <mattoxbeckman@gmail.com>
2014-03-13 19:29:48 +0000

    Sample connection to psql works. Used 'export DATABASE_URL=postgres://localhost/camcall' to connect to my database. Sample data is in 'schema.sql'.

--- server.js
+++ server.js
@@ -2,7 +2,8 @@
 var fs = require('fs'),
     express = require('express'),
     https = require('https'),
-    http = require('http');
+    http = require('http'),
+    pg = require('pg');
 
 
 var privateKey = fs.readFileSync('fakekeys/privatekey.pem').toString(),
@@ -12,6 +13,18 @@ var privateKey = fs.readFileSync('fakekeys/privatekey.pem').toString(),
 var app = express();
 
 app.use(express.static(__dirname));
+
+console.log("Postresql URL is " + process.env.DATABASE_URL);
+
+// Connect to postgres
+
+pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+  client.query('SELECT * FROM users', function(err, result) {
+    done();
+    if(err) return console.error(err);
+    console.log(result.rows);
+  });
+});
 
 var open_port = Number(process.env.PORT || 5000);
 // var secure_port = Number(process.env.PORT || 5001);
