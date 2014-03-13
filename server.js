8943b6d90ea99880337f7f5dece9f2b2a35b96f5
Callahan, Sean P (sc023v) <sc023v@att.com>
2014-03-13 11:10:38 -0500

    port logic

--- server.js
+++ server.js
@@ -14,9 +14,9 @@ var app = express();
 app.use(express.static(__dirname));
 
 var open_port = Number(process.env.PORT || 5000);
-var secure_port = Number(process.env.PORT || 5001);
+// var secure_port = Number(process.env.PORT || 5001);
 
-https.createServer({key: privateKey, cert: certificate}, app).listen(secure_port);
+// https.createServer({key: privateKey, cert: certificate}, app).listen(secure_port);
 http.createServer(app).listen(open_port);
 
 console.log('running on https://localhost:5001 and http://localhost:5000');
