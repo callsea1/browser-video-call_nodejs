e51a2c84ff94b7a1c15fa8229d2cd12a66483af1
Callahan, Sean P (sc023v) <sc023v@att.com>
2014-03-13 10:57:37 -0500

    first commit 2nd time

---
+++ server.js
@@ -0,0 +1,19 @@
+// silly chrome wants SSL to do screensharing
+var fs = require('fs'),
+    express = require('express'),
+    https = require('https'),
+    http = require('http');
+
+
+var privateKey = fs.readFileSync('fakekeys/privatekey.pem').toString(),
+    certificate = fs.readFileSync('fakekeys/certificate.pem').toString();
+
+
+var app = express();
+
+app.use(express.static(__dirname));
+
+https.createServer({key: privateKey, cert: certificate}, app).listen(5001);
+http.createServer(app).listen(5000);
+
+console.log('running on https://localhost:5001 and http://localhost:5000');
